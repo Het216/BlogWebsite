6ecfd82c1376ead40b10ab843d8db91c8fd4ecb2
Het216 <patelhet2104@gmail.com>
2024-11-22 21:41:34 +0530

    end of Auth.js

--- blog/src/appwrite/auth.js
+++ blog/src/appwrite/auth.js
@@ -2,34 +2,57 @@ import conf from "../config/config";
 import { Client, Account, ID } from "appwrite";
 
 
-export class AuthService{
-    client=new Client();
+export class AuthService {
+    client = new Client();
     account;
 
-    constructor(){
+    constructor() {
         this.client
             .setEndpoint(conf.appwriteUrl)
             .setProject(conf.appwriteProject);
         this.account = new Account(this.client);
     }
 
-    async createAccount({email,password,name}){
+    async createAccount({ email, password, name }) {
         try {
-           const userAccount= await this.account.create(ID.unique(),email,password,name);
-          
-           if (userAccount) {
-            return userAccount;
-           } else {
-            
-           }
+            const userAccount = await this.account.create(ID.unique(), email, password, name);
+
+            if (userAccount) {
+                //call Login Method
+                return this.login({ email, password });
+            } else {
+                return userAccount
+            }
 
         } catch (error) {
             throw error;
         }
     }
+    async login({ email, password }) {
+        try {
+            return await this.account.createEmailPasswordSession(email, password);
+        } catch (error) {
+
+        }
+    }
+    async getCurrentUser(){
+        try {
+          return  await this.account.get()
+        } catch (error) {
+            throw error
+        }
+        return null;
+    }
+    async logout(){
+        try {
+          await this.account.deleteSessions()  
+        } catch (error) {
+           throw error 
+        }
+    }
 }
 
-const authservice=new AuthService();
+const authservice = new AuthService();
 
 export default authservice
 
